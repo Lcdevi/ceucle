4588aaebafc6a3837c055afb2a9e0f8c567823b0
Lcdevi <caillauxlaura@gmail.com>
2021-05-03 23:00:50 +0200

    add valide email function

--- script/script.js
+++ script/script.js
@@ -1,14 +1,31 @@
 function sendMail(params) {
 	let resetEmail = document.getElementById("email")
-    var tempParams = {
-        email: document.getElementById("email").value
-    };
+
+	var tempParams = {
+			email: document.getElementById("email").value
+	};
+
+	function emailIsValid (email) {
+		return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+	}
+
+	// console.log(resetEmail.value)
+
+	if (emailIsValid(resetEmail.value)) {
 
 		emailjs.send('service_dapgoeh', 'template_0r3ynqe', tempParams)
-			.then((res) => {
-				console.log('success', res.status, res.text);
-			}, (error) => {
-				console.log('FAILED...', error.text)
-			})
-			resetEmail.value = resetEmail.defaultValue;
+		.then((res) => {
+			console.log('success', res.status, res.text);
+		}, (error) => {
+			console.log('FAILED...', error.text)
+		})
+		
+		resetEmail.value = resetEmail.defaultValue;
+
+	} else {
+		alert("Veuillez saisir un email valide")
+	}
+
+
+
 }
